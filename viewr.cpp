47b6cf58cbce884621e521315e7f1ff5de6f6cd0
Pain <agibalov.sd@phystech.edu>
2021-05-04 00:47:05 +0300

    Refactoring code and comments

--- viewr.cpp
+++ viewr.cpp
@@ -1,14 +1,16 @@
 #include <SFML/Graphics.hpp>
+
 using namespace sf;
 
 sf::View view;
 
 void setPlayerCoordinateForView(float x, float y) {
-float tempX = x; float tempY = y;
+    float tempX = x;
+    float tempY = y;
 
 //if (x < 320) tempX = 320;
-//if (y < 240) tempY = 240;//верхнюю сторону
-//if (y > 624) tempY = 624;//нижнюю стороню.для новой карты
+//if (y < 240) tempY = 240;
+//if (y > 624) tempY = 624;
 
-view.setCenter(tempX, tempY);
+    view.setCenter(tempX, tempY);
 }
